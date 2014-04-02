1aa8debc01bdbe2dffa388e88183f32f36fdd374
Conrad Meyer <cse.cem@gmail.com>
2014-04-01 21:45:46 -0400

    A: Store credit: Finish for basic input

--- practice/A_store_credit/main.c
+++ practice/A_store_credit/main.c
@@ -5,11 +5,11 @@
 #include <stdio.h>
 #include <string.h>
 
-#define SCANF(fmt, n, args...)					\
-do {								\
-	int rc = scanf(fmt, args);				\
-	if (rc != n)						\
-		die("scanf: wanted %d, got %d", (int)(n), rc);	\
+#define SCANF(fmt, n, args...)						\
+do {									\
+	int __rc = scanf(fmt, args);					\
+	if (__rc != (n))						\
+		die("scanf: wanted %d, got %d", (int)(n), __rc);	\
 } while (false)
 
 static void
@@ -24,12 +24,39 @@ die(const char *fmt, ...)
 	exit(1);
 }
 
+unsigned prices[2001];
+
 int
 main(void)
 {
-	uintmax_t N;
+	uintmax_t N, i;
 
 	SCANF("%ju", 1, &N);
+
+	for (i = 1; i <= N; i++) {
+		unsigned C, I, j;
+
+		SCANF("%u", 1, &C);
+		SCANF("%u", 1, &I);
+
+		for (j = 1; j <= I; j++) {
+			unsigned P;
+
+			SCANF("%u", 1, &P);
+			prices[j] = P;
+		}
+
+		for (unsigned x = 1; x <= I; x++) {
+			for (unsigned y = x + 1; y <= I; y++) {
+				if (prices[x] + prices[y] == C) {
+					printf("Case #%ju: %u %u\n", i, x, y);
+					goto next_case;
+				}
+			}
+		}
+
+next_case:;
+	}
 
 	return 0;
 }
