44429c528db29b5436666fdf0c9f1b1512ff486c
Conrad Meyer <cse.cem@gmail.com>
2014-04-01 21:25:31 -0400

    bundle.sh: Add testing

--- bundle.sh
+++ bundle.sh
@@ -24,3 +24,16 @@ done
 cmd="gcc -Wall -Wextra -O3 -g -o ${targetbin} ${target}"
 echo "$cmd"
 zsh -c "$cmd" || die "$cmd: error"
+
+for f in test*.in ; do
+    texp="${f/.in/.out}"
+    [ -f "$texp" ] || die "$f: Has no matching output ($exp)"
+
+    tmpf="$(mktemp)"
+
+    "./${targetbin}" < "$f" > "$tmpf" || die "${targetbin}: error"
+
+    diff -u "$tmpf" "$texp"
+    cmp --quiet "$tmpf" "$texp" || echo "$f failed"
+    rm -f "$tmpf"
+done
