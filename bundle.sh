942c0b97ee53ad257bcb5a8d3269686799be1866
Conrad Meyer <cse.cem@gmail.com>
2014-04-12 21:39:40 -0400

    bundle.sh: Skip the bundling, just keep the test running...

--- bundle.sh
+++ bundle.sh
@@ -6,22 +6,9 @@ die() {
 }
 
 targetbin="$(basename "$(pwd)")"
-target="${targetbin}.c"
-rm -f "$target" "$targetbin"
-
-for f in *.c ; do
-    sed -e "s|\\\\n|\\\\\\\\n|" "$f" | while read line ; do
-        if echo "$line" | egrep -q "^#include\s\s*\"[^\"]*\"$" ; then
-            incfn="$(echo "$line" | sed -e "s|^#include\s\s*\"\([^\"]*\)\"$|\1|")"
-            echo "/* $line */" >> "$target"
-            cat "$incfn" >> "$target"
-        else
-            /usr/bin/echo -E "$line" >> "$target"
-        fi
-    done
-done
+rm -f "$targetbin"
         
-cmd="gcc -std=gnu99 -D_GNU_SOURCE -Wall -Wextra -O3 -g -o ${targetbin} ${target}"
+cmd="gcc -std=gnu99 -D_GNU_SOURCE -Wall -Wextra -O3 -g -o ${targetbin} *.c"
 echo "$cmd"
 zsh -c "$cmd" || die "$cmd: error"
 
@@ -31,6 +18,7 @@ for f in test*.in ; do
 
     tmpf="$(mktemp)"
 
+    echo "\"./${targetbin}\" < \"$f\" > \"$tmpf\""
     "./${targetbin}" < "$f" > "$tmpf" || die "${targetbin}: error during test"
 
     diff -u "$tmpf" "$texp"
