10ac445a1c07a1dd892a8d9183f17ea0638a76e2
Conrad Meyer <cse.cem@gmail.com>
2014-04-25 20:59:01 -0400

    bundle.sh: link lm, explicit about compiler status

--- bundle.sh
+++ bundle.sh
@@ -8,9 +8,10 @@ die() {
 targetbin="$(basename "$(pwd)")"
 rm -f "$targetbin"
         
-cmd="gcc -std=gnu99 -D_GNU_SOURCE -Wall -Wextra -O3 -g -o ${targetbin} *.c"
+cmd="gcc -std=gnu99 -D_GNU_SOURCE -Wall -Wextra -O3 -g -o ${targetbin} *.c -lm"
 echo "$cmd"
 zsh -c "$cmd" || die "$cmd: error"
+echo "============= Compile Passes ============="
 
 for f in test*.in ; do
     texp="${f/.in/.out}"
